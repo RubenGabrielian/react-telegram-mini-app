8788a0c824c6cec39c847543888c9966f5e3482d
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-08-30 10:14:18 +0400

    Adas

--- src/components/layouts/menu.jsx
+++ src/components/layouts/menu.jsx
@@ -4,7 +4,7 @@ import profileIcon from "../../assets/user.png";
 
 export default function Menu () {
     return (
-        <div className={'container mx-auto px-4 absolute bottom-0 pt-4 pb-7 items-center shadow-3xl'}>
+        <div className={'container mx-auto px-6 absolute bottom-0 pt-4 pb-7 items-center shadow-3xl'}>
             <ul className={'list-none flex justify-between'}>
                 <li>
                     <a href="" className={'flex flex-col items-center decoration-0 text-black'}>
